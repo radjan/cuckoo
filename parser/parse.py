05c5cc5d82a6f4c9b579a1b4fdc38901e269404c
radjan <jan.rad@gmail.com>
2013-09-04 01:20:49 +0800

    parse values and put to results

--- parser/parse.py
+++ parser/parse.py
@@ -1,24 +1,39 @@
+# -*- encoding: utf8 -*-
+
 import urllib2
 from bs4 import BeautifulSoup
 
 def _get_by_id(bs, idstr):
     return bs.find_all(id=idstr)[0]
 
-def _child_elements(bs):
-    for c in bs.children:
+def _list_elements(bslist):
+    for c in bslist:
         if c.name != None:
             yield c
 
 def _expend_row(bstr):
     ret = []
-    for c in _child_elements(bstr):
+    for c in _list_elements(bstr.children):
         ret.append(c.string)
     return ret
 
 url = 'http://fubon-ebrokerdj.fbs.com.tw/z/zc/zcp/zcpa/zcpa_2412.djhtm'
+wanted = (u'資產總額', u'負債總額')
 
 bs = BeautifulSoup(urllib2.urlopen(url))
 
-table = _get_by_id(bs, 'oMainTable')
+#table = _get_by_id(bs, 'oMainTable')
 head = _get_by_id(bs, 'oScrollMenu')
-print _expend_row(head)
+periods =  _expend_row(head)[1:]
+
+result = {}
+
+for td in _list_elements(head.next_siblings):
+    items = _expend_row(td)
+    title = items[0].strip()
+    if title in wanted:
+        for i, data in enumerate(items[1:]):
+            result.setdefault(periods[i], {})[title] = int(data.replace(',', ''))
+
+import pprint
+pprint.pprint(result)
