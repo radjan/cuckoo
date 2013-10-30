db3da15503560ceff32d6410e0912dc1a7a682ea
radjan <jan.rad@gmail.com>
2013-10-31 02:40:13 +0800

    calculate average p/e ratio for each category

--- analyzer/calculate_per.py
+++ analyzer/calculate_per.py
@@ -2,7 +2,7 @@
 
 import common
 
-def calculate(stock_no):
+def calculate_per(stock_no, average_data):
     '''
     calculate p/e ratio for last year and last 4Q
     '''
@@ -15,6 +15,7 @@ def calculate(stock_no):
     latest_daily = daily_prices[latest_day]
     latest_price = latest_daily[_field_name(u'股價')]
 
+    average_day = average_data.setdefault(latest_day, {})
     for y, field in ((common.LAST_4Q_YEAR, u'4Q本益比',),
                      (stock_data[common.META][common.LAST_YEAR], u'本益比')):
         try:
@@ -23,15 +24,40 @@ def calculate(stock_no):
                 f = finance[y]
             if f:
                 # per 本益比 = 股價 / 每股盈餘(元)
-                per = latest_price / f[_field_name(u'每股盈餘(元)')]
-                if per < 0:
+                eps = f.get(_field_name(u'每股盈餘(元)'), 0)
+                if eps > 0:
+                    per = latest_price / f[_field_name(u'每股盈餘(元)')]
+                else:
                     per = 0
-                latest_daily[_field_name(field)] = per
+                field_name = _field_name(field)
+                latest_daily[field_name] = per
+
+                # data for average per
+                for postfix in (common.AVG_SUM, common.AVG_COUNT):
+                    k = field_name + postfix
+                    if k not in average_day:
+                        average_day[k] = 0
+                if per > 0:
+                    average_day[field_name + common.AVG_SUM] += per
+                    average_day[field_name + common.AVG_COUNT] += 1
         except Exception as e:
             msg = '%s: %s, per failed: %s %s' % (stock_no, y, type(e), e.message)
             common.report_error(msg)
 
     common.save_stock(stock_no, stock_data)
+
+    return latest_day
+
+def calculate_average_per(average_day, total):
+    average_day[common.TOTAL] = total
+    for field in (u'4Q本益比', u'本益比'):
+        field_name = _field_name(field)
+        count = average_day[field_name + common.AVG_COUNT]
+        if count:
+            avg = average_day[field_name + common.AVG_SUM] / count
+        else:
+            avg = 0
+        average_day[field_name] = avg
 
 def _field_name(key):
     return common.FIELDS[key][0]
@@ -39,13 +65,17 @@ def _field_name(key):
 def main():
     common.save_errors([])
     catalog = common.load_catalog()
+    categories = common.load_categories()
     for category, stocks in catalog.items():
+        average_data = categories.setdefault(category, {})
         for stock_no in stocks:
             try:
-                calculate(stock_no)
+                latest_day = calculate_per(stock_no, average_data)
             except:
                 print stock_no
                 raise
+        calculate_average_per(average_data[latest_day], len(stocks))
+    common.save_categories(categories)
 
 if __name__ == '__main__':
     main()
