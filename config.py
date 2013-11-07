2da6bf00d0a8a34797f78541f3c35a35a13f8311
radjan <jan.rad@gmail.com>
2013-11-08 03:52:04 +0800

    config format updated

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
+KAZUYO_KATSUMA = 'kazuyo_katsuma'
+OLD_BROTHER = 'old_brother'
 
+preferences = {
+    'yuntai': {
+        'email': 'ra2637@gmail.com',
+        'stocks': ['2353', '2401', '2489'],
+        'gold': True,
+    },
 
-yuntai = dict(
-    stocks=['2353', '2401', '2489'],
-    gold=True,
-    )
-
-rad = dict(
-    stocks=['2353', '9941', '2904', '9940', '2373', '9925'],
-    gold=True,
-    )
+    'rad': {
+        'email': 'jan.rad@gmail.com',
+        'stocks': ['2353', '9941', '2904', '9940', '2373', '9925'],
+        'gold': True,
+    },
+}
