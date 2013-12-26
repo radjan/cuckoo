266a074c415906616dd3fc3497982f9c03a9422b
radjan <jan.rad@gmail.com>
2013-12-26 12:11:20 +0800

    add html output

--- notifier/format.py
+++ notifier/format.py
@@ -1,15 +1,25 @@
 # -*- encoding: utf8 -*-
 import sys
 import traceback
+
+from bs4 import BeautifulSoup, Tag
+
 import common
 import config
 from notifier import format_report
 
-def main(fn):
-    format_text(fn)
+_css = """
+<style>
+table { border: 1px solid black;}
+td { border: 1px solid black;}
+</style>
+"""
+
+def main(fn=None, out_format='text'):
+    output(fn, out_format=out_format)
     # TODO format html
 
-def format_text(fn):
+def output(fn, out_format):
     filter_results = common.load_filter_results()
     indicators = common.load_indicator_results()
     categories = common.load_categories()
@@ -23,8 +33,13 @@ def format_text(fn):
             ind[n] = s in v['stocks']
         results.append(format_report.format(sd, ind, categories))
 
-    final = u'\n'.join([u'%s'] * len(results))
 
+    if out_format == 'text':
+        format_text(results, fn)
+    else:
+        format_html(results, fn)
+
+def format_text(results, fn):
     def _to_text(title, values, idents=0):
         new_idents = idents + 4
         if isinstance(values, list):
@@ -39,10 +54,64 @@ def format_text(fn):
 
     texts = tuple((_to_text(*d.output()) for d in results))
 
+    final = u'\n'.join([u'%s'] * len(results))
+    final_str = final % texts
+    _out_file(fn, final_str)
+
+def format_html(results, fn):
+    def _rows_n_cols(dataobj):
+        rows = 1
+        colspan = sub_rows = sub_colspan = 0
+        for d in dataobj.data:
+            if isinstance(d, format_report.PresentData):
+                sub_rows, sub_colspan = _rows_n_cols(d)
+                colspan += sub_colspan
+            else:
+                colspan += 1
+        rows += sub_rows
+        dataobj.rows = rows
+        dataobj.colspan = colspan
+        return rows, colspan
+
+    def _append_tag(bs, parent, name):
+        tag = bs.new_tag(name)
+        parent.append(tag)
+        return tag
+
+    bs = BeautifulSoup("<html><head>%s</head><body></body></html>" % _css, 'lxml')
+    table = _append_tag(bs, bs.body, 'table')
+
+    def _l(dataobj, trs, curr_row):
+        for d in dataobj.data:
+            if isinstance(d, format_report.PresentData):
+                title_td = _append_tag(bs, trs[curr_row], 'td')
+                if d.colspan > 0:
+                    title_td['colspan'] = d.colspan
+                title_td.append(d.title)
+                _l(d, trs, curr_row+1)
+            else:
+                title_td = _append_tag(bs, trs[curr_row], 'td')
+                title_td.append(d[0])
+                rowspan = len(trs) - curr_row - 1
+                if rowspan > 1:
+                    title_td['rowspan'] = rowspan
+                value_td = _append_tag(bs, trs[-1], 'td')
+                value_td.string = unicode(d[1])
+
+    for r in results:
+        rows, colspan = _rows_n_cols(r)
+        trs = []
+        for _row in range(rows+1):
+            tr = _append_tag(bs, table, 'tr')
+            trs.append(tr)
+        _l(r, trs, 0)
+
+    _output_file(fn, bs.prettify())
+
+def _output_file(fn, s):
     import codecs
     f = codecs.open(fn, 'w', 'utf-8')
-    final_str = final % texts
-    f.write(final_str)
+    f.write(s)
 
 def _gather_stocks(filter_results):
     s = []
@@ -56,7 +125,9 @@ def _gather_stocks(filter_results):
 if __name__ == '__main__':
     try:
         fn = sys.argv[1]
-        main(fn)
+        format_ = sys.argv[2] if len(sys.argv) > 2 else 'text'
+        main(fn, format_)
     except Exception as e:
         common.report_error(traceback.format_exc(e))
         raise
+
