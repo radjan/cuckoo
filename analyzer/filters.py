4117963a7108d34fed5d5c3c406545f986788616
Rad Jan <jan.rad@gmail.com>
2015-03-01 11:15:53 +0800

    Add more messages

--- analyzer/filters.py
+++ analyzer/filters.py
@@ -6,23 +6,31 @@ import common
 import config
 
 
+ERROR = common.report_error
+
+
 class NoData(Exception):
     pass
 
 
-def kazuyo_katsuma(stock_no, stock_data):
+def kazuyo_katsuma(s_no, stock_data):
     finance = stock_data[common.FINANCE]
     meta = stock_data[common.META]
     if common.LAST_YEAR not in meta:
-        raise NoData('missing last_year finance report')
+        m = '[KK] %s: missing last_year finance report' % s_no
+        ERROR(m)
+        raise NoData(m)
     wanted = (common.LAST_4Q_YEAR, meta[common.LAST_YEAR])
     # wanted = (meta[common.LAST_YEAR],)
     for y in wanted:
         if not y:
-            raise NoData("Missing last_year report")
-        if not enough_value(finance[y], u'本期稅後淨利', 0.0, period=y):
+            m = "[KK] %s: Missing last_year report" % s_no
+            ERROR(m)
+            raise NoData(m)
+        if not enough_value(finance[y], u'本期稅後淨利', 0.0, period=y,
+                            s_no=s_no):
             return False
-        if not negative_accrual(finance[y], period=y):
+        if not negative_accrual(finance[y], period=y, s_no=s_no):
             return False
     quarters = meta[common.QUARTERS]
     var_roa = common.field_var(u'總資產報酬率')
@@ -32,47 +40,55 @@ def kazuyo_katsuma(stock_no, stock_data):
         last_qf = finance[last_q]
         if var_roa in qf and var_roa in last_qf:
             return qf[var_roa] - last_qf[var_roa] > 0
-        raise NoData('Missing ROA data for %s or %s' % (q, last_q))
+        m = '[[KK] %s: Missing ROA data for %s or %s' % (s_no, q, last_q)
+        ERROR(m)
+        raise NoData(m)
 
     try:
         return continous(quarters[:5], roa_growth)
     except Exception, e:
-        print stock_no, e
+        print s_no, e
         raise
 
 
-def old_brother(stock_no, stock_data):
+def old_brother(s_no, stock_data):
     finance = stock_data[common.FINANCE]
     meta = stock_data[common.META]
     if common.LAST_YEAR not in meta:
+        m = '[OB] %: missing last_year finance report' % s_no
         raise NoData('missing last_year finance report')
     last_year = meta[common.LAST_YEAR]
 
     wanted = (common.LAST_4Q_YEAR, last_year)
     for y in wanted:
         if not y:
-            raise NoData("Missing last_year report")
-        if not enough_value(finance[y], u'本期稅後淨利', 0.0, period=y):
+            m = "[OB] %s Missing last_year report" % s_no
+            ERROR(m)
+            raise NoData(m)
+        if not enough_value(finance[y], u'本期稅後淨利', 0.0, period=y,
+                            s_no=s_no):
             return False
         if not enough_value(finance[y], u'權責發生額', 0, period=y,
-                            reverse=True):
+                            reverse=True, s_no=s_no):
             return False
 
     daily_reports = stock_data[common.DAILY]
     latest_day = sorted(daily_reports.keys(), reverse=True)[0]
     latest_report = daily_reports[latest_day]
     if not enough_value(latest_report, u'殖利率',
-                        0.05, period=latest_day):
+                        0.05, period=latest_day, s_no=s_no):
         return False
 
     recent_years = meta[common.ANNUALS][:3]
     recent_years.insert(0, common.LAST_4Q_YEAR)
     for y in recent_years:
         report = finance[y]
-        if not all((enough_value(report, u'營業毛利率', 0.3, period=y),
+        if not all((enough_value(report, u'營業毛利率', 0.3, period=y,
+                                 s_no=s_no),
                     enough_value(report, u'負債比率', 0.3, period=y,
-                                 reverse=True),
-                    enough_value(report, u'每股盈餘(元)', 0, period=y))):
+                                 reverse=True, s_no=s_no),
+                    enough_value(report, u'每股盈餘(元)', 0, period=y,
+                                 s_no=s_no))):
             return False
 
     def loose_eps_growth(y1, _y2, y3):
@@ -84,21 +100,26 @@ def old_brother(stock_no, stock_data):
     return continous(recent_years, loose_eps_growth, window=3)
 
 
-def negative_accrual(finance_report, period=None):
+def negative_accrual(finance_report, period=None, s_no=None):
     accrual = common.field_var(u'權責發生額')
     if accrual in finance_report:
         return finance_report[accrual] < 0
-    raise NoData('Missing accrual data for %s' % period)
+    m = '[negative_accrual] %s: Missing accrual data for %s' % (s_no, period)
+    ERROR(m)
+    raise NoData(m)
 
 
-def enough_value(finance_report, field, threshold, period=None, reverse=False):
+def enough_value(finance_report, field, threshold, period=None, reverse=False,
+                 s_no=None):
     var_name = common.field_var(field)
     if var_name in finance_report:
         ret = finance_report[var_name] >= threshold
         if reverse:
             ret = not ret
         return ret
-    raise NoData('Missing %s data for %s' % (var_name, period))
+    m = '[enough_value] %s: Missing %s(%s) data for %s' % (s_no, field, var_name, period)
+    ERROR(m)
+    raise NoData(m)
 
 
 def continous(l, func, window=2):
@@ -113,19 +134,19 @@ def main():
     result = {}
     catalog = common.load_catalog()
     for category, stocks in catalog.items():
-        for stock_no in stocks:
-            stock_data = common.load_stock(stock_no)
+        for s_no in stocks:
+            stock_data = common.load_stock(s_no)
             for filter_name, filter_func in filters.items():
                 data = result.setdefault(filter_name, {})
                 data.setdefault(common.KEY_STOCKS, [])
                 data.setdefault(common.KEY_MISSING_DATA, [])
                 try:
-                    if filter_func(stock_no, stock_data):
-                        data[common.KEY_STOCKS].append(stock_no)
+                    if filter_func(s_no, stock_data):
+                        data[common.KEY_STOCKS].append(s_no)
                 except NoData as e:
-                    data[common.KEY_MISSING_DATA].append(stock_no)
+                    data[common.KEY_MISSING_DATA].append(s_no)
                 except Exception as e:
-                    print stock_no
+                    print s_no
                     raise
 
     for k, v in result.items():
@@ -141,5 +162,5 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        common.report_error(traceback.format_exc(e))
+        ERROR(traceback.format_exc(e))
         raise
