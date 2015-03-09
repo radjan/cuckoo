5abcc3603b6bfef0fe70d3308ca06d13b6ffabac
Rad Jan <jan.rad@gmail.com>
2015-03-09 21:53:15 +0800

    Show the latest period of finance report for each stock

--- show_filter_result.py
+++ show_filter_result.py
@@ -2,14 +2,27 @@
 
 import common
 
+
+def _show(info):
+    return '%s %s(%s/%s)' % info
+
+
 f = common.load_filter_results()
 
 for k, v in f.items():
     print 'filter:', k
-    display = []
+    results = []
     for s_no in sorted(v['stocks']):
         s_info = common.load_stock(s_no)
-        info = (s_no, s_info[common.META][common.META_NAME])
-        display.append(info)
+        meta = s_info[common.META]
+        s_name = meta[common.META_NAME]
+        last_q = meta[common.QUARTERS][0]
+        last_y = meta[common.LAST_YEAR]
+        info = (s_no, s_name, last_q, last_y)
+        results.append(info)
 
-    print '    ', ', '.join(['%s %s' % (info[0], info[1]) for info in display])
+    # Order by (newer LAST_YEAR, newer LAST_QUARTER, smaller STOCK NO)
+    results = sorted(results,
+                     key=lambda x: (x[3]+x[2], 100000 - int(x[0])),
+                     reverse=True)
+    print '    ', ', '.join([_show(info) for info in results])
