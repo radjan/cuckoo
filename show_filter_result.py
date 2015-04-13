f8a6b37a3bd61ef10f18d8d9335ddc1add084b08
Rad Jan <jan.rad@gmail.com>
2015-04-13 06:13:43 +0000

    Show filtered results grouped by finance report

--- show_filter_result.py
+++ show_filter_result.py
@@ -4,25 +4,25 @@ import common
 
 
 def _show(info):
-    return '%s %s(%s/%s)' % info
-
+    return '%s %s' % info
 
 f = common.load_filter_results()
 
 for k, v in f.items():
     print 'filter:', k
-    results = []
+    results = {}
     for s_no in sorted(v['stocks']):
         s_info = common.load_stock(s_no)
         meta = s_info[common.META]
         s_name = meta[common.META_NAME]
         last_q = meta[common.QUARTERS][0]
         last_y = meta[common.LAST_YEAR]
-        info = (s_no, s_name, last_q, last_y)
-        results.append(info)
+        info = (s_no, s_name)
+        fin_info = 'Y:%s Q:%s' % (last_y, last_q)
+        if fin_info not in results:
+            results[fin_info] = []
+        results[fin_info].append(info)
 
-    # Order by (newer LAST_YEAR, newer LAST_QUARTER, smaller STOCK NO)
-    results = sorted(results,
-                     key=lambda x: (x[3]+x[2], 100000 - int(x[0])),
-                     reverse=True)
-    print '    ', ', '.join([_show(info) for info in results])
+    for fin_key in sorted(results.keys(), reverse=True):
+        print '  %s' % fin_key
+        print '    %s' % ', '.join([_show(info) for info in results[fin_key]])
